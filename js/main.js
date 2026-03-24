6a4366ed366a631579d9aa7bcfd9fd07e25e80ad
Dinar <dinaraznaev370@gmail.com>
2026-03-24 15:21:07 +0500

    feat: реализовал страницу О магазине

--- js/main.js
+++ js/main.js
@@ -109,3 +109,21 @@ async function addToCartAPI(productId) {
         throw new Error('Ошибка при добавлении товара. Попробуйте перезагрузить страницу');
     }
 }
+
+// Функция списка FAQ
+document.querySelectorAll('.faq__question').forEach(button => {
+    button.addEventListener('click', () => {
+        const item = button.parentElement;
+        const isActive = item.classList.contains('is-active');
+        
+        document.querySelectorAll('.faq__item').forEach(faqItem => {
+            faqItem.classList.remove('is-active');
+            faqItem.querySelector('.faq__question').setAttribute('aria-expanded', 'false');
+        });
+        
+        if (!isActive) {
+            item.classList.add('is-active');
+            button.setAttribute('aria-expanded', 'true');
+        }
+    });
+});
